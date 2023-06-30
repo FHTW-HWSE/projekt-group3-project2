7acacad6812f3942206d9cc4972dfc2ef68e33bc
BIC-091 <131018454+BIC-091@users.noreply.github.com>
2023-06-30 21:45:53 +0200

    Delete build directory

--- build/CMakeFiles/program_library.dir/src/guest.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/program_library.dir/src/guest.cpp.o: \
-  /Users/macbook/Documents/FH/HWSE/projekt-group3-project2/src/guest.cpp
--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/macbook/Documents/FH/HWSE/projekt-group3-project2
-# Build directory: /Users/macbook/Documents/FH/HWSE/projekt-group3-project2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/macbook/Documents/FH/HWSE/projekt-group3-project2/build/program_tests_include-b12d07c.cmake")
